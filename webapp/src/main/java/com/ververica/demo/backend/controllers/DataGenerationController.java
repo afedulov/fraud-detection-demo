6ab8bb7c45553b0eaf33d7037929b66b11cdaa14
Alexander Fedulov <1492164+afedulov@users.noreply.github.com>
2020-01-12 16:56:18 +0100

    Add ASL2.0 copyright

--- webapp/src/main/java/com/ververica/demo/backend/controllers/DataGenerationController.java
+++ webapp/src/main/java/com/ververica/demo/backend/controllers/DataGenerationController.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ververica.demo.backend.controllers;
 
 import com.ververica.demo.backend.datasource.DemoTransactionsGenerator;
@@ -22,12 +39,14 @@ public class DataGenerationController {
   private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
 
   private ExecutorService executor = Executors.newSingleThreadExecutor();
-  private boolean generatingTemperature = false;
   private boolean generatingTransactions = false;
   private boolean listenerContainerRunning = true;
 
   @Value("${kafka.listeners.transactions.id}")
   private String transactionListenerId;
+
+  @Value("${transactionsRateDisplayLimit}")
+  private int transactionsRateDisplayLimit;
 
   @Autowired
   public DataGenerationController(
@@ -70,8 +89,7 @@ public class DataGenerationController {
 
     MessageListenerContainer listenerContainer =
         kafkaListenerEndpointRegistry.getListenerContainer(transactionListenerId);
-    // TODO: extract to config
-    if (speed > 50) {
+    if (speed > transactionsRateDisplayLimit) {
       listenerContainer.stop();
       listenerContainerRunning = false;
     } else if (!listenerContainerRunning) {
